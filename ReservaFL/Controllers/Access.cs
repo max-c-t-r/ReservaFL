e0af3cc59077f4a3051decbfb5bf704d039fd8ed
Max CTR <68170318+max-c-t-r@users.noreply.github.com>
2023-08-13 22:44:37 -0500

    primer commit

---
+++ ReservaFL/Controllers/Access.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ReservaFL.Controllers
+{
+    public class Access : Controller
+    {
+        public IActionResult Login()
+        {
+            return View();
+        }
+        public IActionResult Register()
+        {
+            return View();
+        }
+    }
+}
