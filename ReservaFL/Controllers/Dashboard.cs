94c5579777c2ce1ef3d999f5464d7e1923decf7e
Max CTR <68170318+max-c-t-r@users.noreply.github.com>
2023-08-15 17:28:48 -0500

    segundo commit

---
+++ ReservaFL/Controllers/Dashboard.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ReservaFL.Controllers
+{
+    public class Dashboard : Controller
+    {
+        public IActionResult User()
+        {
+            return View();
+        }
+
+
+        public IActionResult Admin()
+        {
+            return View();
+        }
+    }
+}
